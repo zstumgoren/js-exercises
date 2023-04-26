87aec46ab7d44c021617389af4a69a08b91d05a5
Serdar Tumgoren <zstumgoren@gmail.com>
2023-04-26 08:35:51 -0700

    add page1.html and update ex1.js

--- exercises/1/ex1.js
+++ exercises/1/ex1.js
@@ -3,4 +3,7 @@
 (function (){ 
   var h1 = document.querySelector('h1');
   h1.innerText = "Testing, 1...2...";
+  h1.style.color = "blue";
+  h1.style.backgroundColor = "gray";
+  document.querySelector('body').style.backgroundColor = "orange";
 })()
