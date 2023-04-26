3b9e5fbea39a29d74f6f15adc2aec670d3781c21
Serdar Tumgoren <zstumgoren@gmail.com>
2023-04-26 08:44:10 -0700

    align html and js filenames for page1

--- exercises/1/ex1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-(function (){ 
-  var h1 = document.querySelector('h1');
-  h1.innerText = "Testing, 1...2...";
-  h1.style.color = "blue";
-  h1.style.backgroundColor = "gray";
-  document.querySelector('body').style.backgroundColor = "orange";
-})()
---
+++ exercises/1/page1.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var h1 = document.querySelector('h1');
+h1.innerText = "Testing, 1...2...";
+h1.style.color = "blue";
+h1.style.backgroundColor = "gray";
+document.querySelector('body').style.backgroundColor = "orange";
